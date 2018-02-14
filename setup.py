9f418d4c691243ef5535954c8fdc52090383ed7f
KenLauLab <charles.a.herring@vanderbilt.edu>
2018-02-14 12:03:33 -0600

    first push

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup( name='NVR',
+       version='0.0.0',
+       description='python implementation of gene feature selection used in Welch et al.,2016',
+       url='https://github.com/KenLauLab/NVR',
+       author='Bob Chen',
+       author_email='bob.chen@vanderbilt.edu',
+       packages=['nvr'],
+       install_requires=[
+           'numpy>=1.11.0',
+           'pandas>=0.17.1',
+           'scipy>=0.17.0',
+           'networkx',
+           
+       ],
+     )
+
